35177de682f3f56ed840c2ab52a7c8f40ad31cd5
Redpike <rsokulski@op.pl>
2018-12-05 21:13:45 +0100

    Migrate 2 separated modules to one project, configure building to one WAR file

---
+++ README.md
@@ -0,0 +1,10 @@
+## CookBook
+
+We have two separated modules:
+* `front-end` - using Angular Framework for creating GUI,
+* `back-end` - using Spring Boot for server-side and REST API
+
+## Contributors
+
+* [Redpike](https://github.com/Redpike)
+* [KurdDt](https://github.com/KurdTt)
--- src/main/java/pl/com/redpike/cookbook/CookbookApplication.java
+++ src/main/java/pl/com/redpike/cookbook/CookbookApplication.java
@@ -1,12 +1,12 @@
-package pl.com.redpike.cookbookserver;
+package pl.com.redpike.cookbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CookbookServerApplication {
+public class CookbookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CookbookServerApplication.class, args);
+		SpringApplication.run(CookbookApplication.class, args);
 	}
 }
